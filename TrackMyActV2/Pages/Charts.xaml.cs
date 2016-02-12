81d97405e4f5854cc58946aeee1c56fc8d86b676
agangal <ashish.gangal1990@gmail.com>
2016-02-12 14:33:31 -0800

    Charts worls well now. Thanks to SyncFusion :D

--- TrackMyActV2/Pages/Charts.xaml.cs
+++ TrackMyActV2/Pages/Charts.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -14,8 +15,8 @@ using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using WinRTXamlToolkit.Controls.DataVisualization.Charting;
-
+using TrackMyActV2.Models;
+using Syncfusion.UI.Xaml.Charts;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace TrackMyActV2.Pages
@@ -25,42 +26,78 @@ namespace TrackMyActV2.Pages
     /// </summary>
     public sealed partial class Charts : Page
     {
+       
+        public ObservableCollection<GoldDemand> Demands { get; set; }
+        private timerToChartsTransfer transfer;
+
         public Charts()
         {
             this.InitializeComponent();
-            Loaded += Charts_Loaded;
+            transfer = new timerToChartsTransfer();
         }
 
-        public class temp
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            public DateTime seconds;
-            public int count;
+            Demands = new ObservableCollection<GoldDemand>();
+            transfer = (timerToChartsTransfer)e.Parameter;
+            var activity = transfer.trackact.activity[(int)transfer.activity_pos];
+            Demands.Clear();
+            int i = 0;
+            foreach(var act in activity.timer_data)
+            {
+                GoldDemand dem = new GoldDemand();
+                dem.Demand = i + 1;
+                dem.seconds = act.time_in_seconds;
+                Demands.Add(dem);
+                i++;
+            }
+            this.DataContext = this;
+            i = 0;
+        }
+        public class GoldDemand
+        {
+            public int Demand { get; set; }
+            public long seconds { get; set; }
+            
         }
         /// <summary>
-        /// Code taken from : https://social.msdn.microsoft.com/Forums/en-US/e5a7955c-0a14-4ec8-99cb-b2dfe178cf4b/charting-datetimeaxis-range?forum=silverlightcontrols
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+         /// Code taken from : https://social.msdn.microsoft.com/Forums/en-US/e5a7955c-0a14-4ec8-99cb-b2dfe178cf4b/charting-datetimeaxis-range?forum=silverlightcontrols
+         /// </summary>
+         /// <param name="sender"></param>
+         /// <param name="e"></param>
         private void Charts_Loaded(object sender, RoutedEventArgs e)
         {
-            //myChart.DataContext = new KeyValuePair<int, DateTime>[]
-            //{
-            //    new KeyValuePair<int, DateTime>(1, DateTime.Today.AddSeconds(10)),
-            //    new KeyValuePair<int, DateTime>(2, DateTime.Today.AddSeconds(50)),
-            //    new KeyValuePair<int, DateTime>(3, DateTime.Today.AddSeconds(30))
-            //};
+           /* SfChart chart = new SfChart();
             
-            List<temp> templist = new List<temp>();
-            templist.Add(new temp() { seconds = DateTime.Today.AddSeconds(20), count = 1 });
-            templist.Add(new temp() { seconds = DateTime.Today.AddSeconds(10), count = 2 });
-            templist.Add(new temp() { seconds = DateTime.Today.AddSeconds(6), count = 3 });
-            templist.Add(new temp() { seconds = DateTime.Today.AddSeconds(100), count = 4 });
-            (myChart.Series[0] as ColumnSeries).ItemsSource = templist;
-            DateTimeAxis axis = (DateTimeAxis)myChart.Axes[0];
-            axis.Minimum = DateTime.Today.AddSeconds(10);
-            axis.Maximum = DateTime.Today.AddSeconds(100);
-            axis.Interval = 5;
-            axis.IntervalType = DateTimeIntervalType.Seconds;
+            chart.HorizontalAlignment = HorizontalAlignment.Center;
+            chart.VerticalAlignment = VerticalAlignment.Center;
+            chart.Header = "Time spent on " + transfer.trackact.activity[(int)transfer.activity_pos];
+            chart.Height = 300;
+            chart.Width = 500;
+
+            NumericalAxis primaryCategoryAxis = new NumericalAxis();
+            primaryCategoryAxis.Header = "Time (in seconds)";
+
+            chart.PrimaryAxis = primaryCategoryAxis;
+
+            NumericalAxis secondaryNumericalAxis = new NumericalAxis();
+            secondaryNumericalAxis.Header = "Event";
+
+            chart.SecondaryAxis = secondaryNumericalAxis;
+
+            ColumnSeries series1 = new ColumnSeries();
+            series1.EnableAnimation = true;
+            series1.ShowTooltip = true;
+            series1.Label = "Time (in seconds)";
+            series1.ItemsSource = Demands;
+            series1.XBindingPath = "Demand";
+            series1.YBindingPath = "seconds";           
+
+            chart.Series.Add(series1);
+         
+            chart.Legend = new ChartLegend() { Visibility = Visibility.Visible };
+            MainGrid.Children.Add(chart); */
+            
         }        
 
     }
